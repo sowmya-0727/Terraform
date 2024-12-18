822f122e43496f45a3bead10ae97adb2b1f5cbfc
sowmya-0727 <jyothisowmya7727@gmail.com>
2024-12-18 13:18:33 +0530

    Create main.tf

---
+++ Storage Account/main.tf
@@ -0,0 +1,21 @@
+# Configure the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+
+  tags = var.tags
+}
